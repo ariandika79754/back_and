9d7265746aa5dd3324d1937a8078c3f25ce82f3f
Ari Andika <ariandika79754@gmail.com>
2024-08-08 13:41:18 +0700

    update materi database

--- database.sql
+++ database.sql
@@ -16,7 +16,8 @@ CREATE TABLE class (
     id INT AUTO_INCREMENT PRIMARY KEY, -- Kolom ID sebagai primary key yang auto increment
     nama VARCHAR(255) NOT NULL,        -- Kolom nama dengan tipe data VARCHAR
     teacher_id INT not null,         -- Kolom teacher_id dengan tipe data VARCHAR
-    constraint Foreign key (teacher_id) references teacher (id) on Delete cascade
+    constraint Foreign key (teacher_id) references teacher (id) on Delete cascade -- fungsi cascade  delete
+    -- apabila ada data guru yang di delete atau di update maka akan otomatasi berubah dan terhapus di table class 
 );
 
 CREATE TABLE teacher (
@@ -41,3 +42,19 @@ WHERE id = 1; -- Memperbarui umur menjadi 30 untuk data dengan ID 1
 -- Delete (Menghapus data dari tabel)
 DELETE FROM students 
 WHERE id = 1; -- Menghapus data dengan ID 1 dari tabel
+
+
+--dalam proses pengambilan data dari 2 atau lebih table kita perlu melakukan pemanggilan
+-- column gunakan  as berfungsi untuk menjadi alias atau pengganti dari nama column dari kasus
+-- kita mencoba mengambil data dari table class dengan menggunakan inner join dimana inner join membutuhkan
+-- foreign key sebagai kunci asing atau sebagai izin untuk mengakses table
+SELECT 
+    class.nama as nama_kelas,
+    teacher.nama as nama_guru
+FROM 
+    class 
+INNER JOIN 
+    teacher 
+ON 
+    class.teacher_id = teacher.id;
+
